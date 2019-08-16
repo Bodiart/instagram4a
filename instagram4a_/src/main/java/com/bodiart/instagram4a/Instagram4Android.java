974135c86c9e2254ab7e977603dd7ab2585ffdb3
Bogdan <b.hochachko@ideil.com>
2019-08-16 16:39:34 +0300

    OkHttp client, increased timeout

--- instagram4a_/src/main/java/com/bodiart/instagram4a/Instagram4Android.java
+++ instagram4a_/src/main/java/com/bodiart/instagram4a/Instagram4Android.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
@@ -73,7 +74,11 @@ public class Instagram4Android {
                         return validCookies;
                     }
                 })
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
                 .build();
+
+
 
     }
 
